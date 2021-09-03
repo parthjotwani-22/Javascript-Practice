a5314c2e3fb0b2ff9ef2569587cb41f50a4e247d
parthjotwani-22 <parth_jotwani@hotmail.com>
2021-09-03 12:37:52 +0530

    Datatypes

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-console.log('This is my first piece of js code!! yay');
-console.warn('This is a warning');
-console.error("THis is an error");
+// console.log('This is my first piece of js code!! yay');
+// console.warn('This is a warning');
+// console.error("THis is an error");
+// console.log(false,123456,"hello",{name:"prime"});
+
+let age =22;
+let Name="op";
+let option =true;
+console.log(typeof age);
+console.log(Name);
+let brand;
+console.log( typeof brand);
+let arr =[1,2,34,5];
+arr.push(6);
+console.log(arr);
+console.log(typeof option);
