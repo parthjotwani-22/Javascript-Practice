32d44659f134b5853afc01efa55888b21c69c48e
parthjotwani-22 <parth_jotwani@hotmail.com>
2021-09-10 17:00:48 +0530

    js basic operators and statments

--- app.js
+++ app.js
@@ -1,16 +1,60 @@
-const  a = 8;
-const  b=5;
+//arithmetic operators
 
-console.log(a+b);
-console.log(a-b);
-console.log(a**b);
-console.log(a/b);
-console.log(a%b);
 
-let c=9;
-c+=1;
-console.log(c);
-c*=2;
-console.log(c);
-c**=2;
-console.log(c);
+// const  a = 8;
+// const  b=5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a**b);
+// console.log(a/b);
+// console.log(a%b);
+
+
+//assignmet operators
+// let c=9;
+// c+=1;
+// console.log(c);
+// c*=2;
+// console.log(c);
+// c**=2;
+// console.log(c);
+
+//conditional statements
+
+// const a=99;
+
+// if(typeof(a)===number){
+// if(a==0){
+//     console.log("Given number is zero");
+// }
+// else if (a%2==0){
+//     console.log("Even number");
+// }
+// else{
+//     console.log("odd number");
+// }
+// }
+// else{
+//     console.log("It")
+// }
+
+
+//comparision operators
+
+// const a=2;
+// const b=9;
+
+// console.log("a<b",a<b);
+// console.log("a>b",a>b);
+// console.log("a>=b",a>=b);
+// console.log("a<=b",a<=b);
+// console.log("a==b",a==b);
+
+//logical operators
+
+// console.log(true && false);
+// console.log(false && false);
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || false);
