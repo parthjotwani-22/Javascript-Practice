c1c04b5bdca9829c2227fc888ea0a0eeae2161dc
parthjotwani-22 <parth_jotwani@hotmail.com>
2021-09-11 22:22:27 +0530

    dom basics

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-// console.log('This is my first piece of js code!! yay');
-// console.warn('This is a warning');
-// console.error("THis is an error");
-// console.log(false,123456,"hello",{name:"prime"});
+// const element=document.getElementById('selectme')
+// const hr =document.getElementsByClassName('heading')
+// const element=document.getElementsByTagName('h2')
+const element=document.querySelector('#selectme')
 
-let age =22;
-let Name="op";
-let option =true;
-console.log(typeof age);
-console.log(Name);
-let brand;
-console.log( typeof brand);
-let arr =[1,2,34,5];
-arr.push(6);
-console.log(arr);
-console.log(typeof option);
+//console.log(hr);
+console.log(element);
