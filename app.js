9e2aa95df1d479524208f881c49ef2c06affc05d
parthjotwani-22 <parth_jotwani@hotmail.com>
2021-09-12 10:18:53 +0530

    more element methods

--- app.js
+++ app.js
@@ -4,4 +4,17 @@
 const element=document.querySelector('#selectme')
 
 //console.log(hr);
-console.log(element);
+// console.log(element);
+// element.innerHTML="HI I am parth"
+// element.style.color="red"
+// element.style.fontSize="40px"
+
+// console.log(element.classList)
+// element.classList.add('five')
+// element.classList.remove('one')
+// console.log(element.classList)
+
+// const body =document.querySelector('body')
+//  console.log(body.classList.toggle('dark'))
+
+console.log(element.classList.contains('one'))
